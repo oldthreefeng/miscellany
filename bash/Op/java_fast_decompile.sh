e6204da23dd05eedef9df34503d2662f107893d9
xiaodong <jokimina@163.com>
2018-12-09 23:48:22 +0800

    java fast decompile

---
+++ bash/Op/java_fast_decompile.sh
@@ -0,0 +1,2 @@
+wget http://www.benf.org/other/cfr/cfr_0_130.jar
+java -jar cfr_0_130.jar BankCardRest.class  --methodname validatePrepareava -jar cfr_0_130.jar BankCardRest.class  --methodname validatePrepare
